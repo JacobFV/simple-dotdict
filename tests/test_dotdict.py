6a8915dd3128ba310244c3c436250d161a2333d2
Jacob Valdez <jacobfv123@gmail.com>
2023-08-29 13:19:33 -0500

    added tests

--- tests/test_dotdict.py
+++ tests/test_dotdict.py
@@ -11,3 +11,85 @@ def test_get_attribute():
 
     # Assert
     assert result == "value"
+
+
+def test_set_attribute():
+    # Arrange
+    d = dotdict()
+
+    # Act
+    d.key = "value"
+
+    # Assert
+    assert d["key"] == "value"
+
+
+def test_delete_attribute():
+    # Arrange
+    d = dotdict()
+    d.key = "value"
+
+    # Act
+    del d.key
+
+    # Assert
+    assert "key" not in d
+
+
+def test_exists_attribute():
+    # Arrange
+    d = dotdict()
+    d.key = "value"
+
+    # Act
+    exists = hasattr(d, "key")
+
+    # Assert
+    assert exists == True
+
+
+def test_get_item():
+    # Arrange
+    d = dotdict()
+    d["key"] = "value"
+
+    # Act
+    result = d["key"]
+
+    # Assert
+    assert result == "value"
+
+
+def test_set_item():
+    # Arrange
+    d = dotdict()
+
+    # Act
+    d["key"] = "value"
+
+    # Assert
+    assert d.key == "value"
+
+
+def test_delete_item():
+    # Arrange
+    d = dotdict()
+    d["key"] = "value"
+
+    # Act
+    del d["key"]
+
+    # Assert
+    assert "key" not in d
+
+
+def test_exists_item():
+    # Arrange
+    d = dotdict()
+    d["key"] = "value"
+
+    # Act
+    exists = "key" in d
+
+    # Assert
+    assert exists == True
